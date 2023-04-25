829ea5670bbd74ee7119c4339831e33faec42162
Sion Daniel <sirdanie@ucsc.edu>
2023-04-25 10:31:41 -0700

    Lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,8 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables
+// Author: Sion Daniel
+// Date: 04/25/2023
 
 // Constants
+make = "Mazda";
+model = "v3";
+color = "Dark Blue";
+year = 2008;
+ownIt = true;
+
+// Calculate
+age = 2023 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // Functions
 
