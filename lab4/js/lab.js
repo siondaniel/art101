d46ff16dc721e7ee0c0d1f738ffe070b244057fa
Sion Daniel <sirdanie@ucsc.edu>
2023-04-24 19:49:07 -0700

    Combining docs w partner

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js
+// This file will recreate a version of Hangman.
+// Author: Hilton Mai, Sion Daniel
+// Date: April 24, 2023
 
 // Constants
 
@@ -15,6 +16,24 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+
+  // Hangman Pseudocode
+  // Define an array of words to choose from
+  // Choose a random word from the array
+  // Create a variable to store number of attempts left
+  // Create a variable to store the current state of the game
+  // Loop until the game is over
+      // Display current state of the word with masked letters
+      // Display the masked word and the attempts left
+      // Prompt the user to enter a letter
+      // If the guessed letter is correct
+          // Add the correct guess to the guesses array
+          // Check if the player has won
+      // If not
+          // Reduce the attempts left
+          // Check if the player has lost
+  // Display the final result
+
 }
 
 // let's get this party started
