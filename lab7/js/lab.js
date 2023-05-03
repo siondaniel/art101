b6b71b0c303d0d3cc31b01e5ba9f145c66a9f997
Sion Daniel <sirdanie@ucsc.edu>
2023-05-03 11:13:52 -0700

    Sort Updates + Screenshot

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,6 +2,9 @@
 // Author: Sion Daniel
 // Date: 05/03/2023
 
+// Constants
+var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+
 // Functions
 
 // I took the below code from the screenshot in the lab instructions
@@ -9,14 +12,14 @@
 
 // sortUserName - a function that takes user input and sorts the letters
 // of their name
-function sortUserName() {
-  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+function sortUserName(userName) {
   console.log("username=", userName);
   // split string to array
   var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
-  // sort the array
-  var nameArraySort = nameArray.sort();
+  // sort the array using localeCompare: 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  var nameArraySort = nameArray.sort((a, b) => a.localeCompare(b));
   console.log("nameArraySort =", nameArraySort);
   // join array back to a string
   var nameSorted = nameArraySort.join('')
@@ -27,4 +30,4 @@ function sortUserName() {
 // let's get this party started
 // Output
 document.writeln("Oh hey, I've fixed your name: ",
-  sortUserName(), "</br>");
+  sortUserName(userName), "</br>");
